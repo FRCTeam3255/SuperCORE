82145bbffd4b0fc36f5d891f8ebd9c621ca31040
Tayler Uva <8679670+TaylerUva@users.noreply.github.com>
2021-05-14 12:30:52 -0700

    improved web portion

--- create_dir.sh
+++ create_dir.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
-DIR="."
-(
-	echo -e "<html>\n<body>\n<h1>SuperCORE Maven Repo</h1>\n<hr/>\n<pre>"
-	ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "index.html" | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }' 
-echo -e "</pre>\n</body>\n</html>"
-) > "${DIR}/index.html"
 for DIR in $(find ./releases -type d); do
   (
-    echo -e "<html>\n<body>\n<h1>SuperCORE Maven Repo</h1>\n<hr/>\n<pre>"
+    echo -e "<body>
+	<h1>SuperCORE Maven Repo</h1>
+	<h2><a href=\"/\">Home</a>
+		<a href=\"/releases/com/frcteam3255/supercore/\">Releases</a>
+		<a href=\"https://github.com/FRCTeam3255/SuperCORE\">Source</a>
+	</h2>
+	<hr />
+	<pre>"
     ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "index.html" | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }' 
     echo -e "</pre>\n</body>\n</html>"
+    echo -e "<footer>
+	<hr />
+	<pre>
+Created by <a href=\"https://supernurds.com\" target=\"_blank\">FRC Team 3255 - SuperNURDs</a>
+	</pre>
+</footer>"
   ) > "${DIR}/index.html"
 done

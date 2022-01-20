009bae05c96978a41bb7aa0678f558be4310854e
Tayler Uva <8679670+TaylerUva@users.noreply.github.com>
2022-01-19 18:30:10 -0800

    published v0.0.5

--- releases/com/frcteam3255/supercore/javadoc-latest/package-search-index.js
+++ releases/com/frcteam3255/supercore/javadoc-latest/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.frcteam3255.joystick"},{"l":"com.frcteam3255.preferences"},{"l":"com.frcteam3255.utils"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.frcteam3255.components"},{"l":"com.frcteam3255.joystick"},{"l":"com.frcteam3255.preferences"},{"l":"com.frcteam3255.utils"}]

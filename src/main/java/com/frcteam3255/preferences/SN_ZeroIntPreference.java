0dc5e7e4f3417ca7bac0e9b4fdc5913b63d258f7
IanSzalai <60115484+IanSzalai@users.noreply.github.com>
2022-02-18 19:00:21 -0800

    spotless apply

--- src/main/java/com/frcteam3255/preferences/SN_ZeroIntPreference.java
+++ src/main/java/com/frcteam3255/preferences/SN_ZeroIntPreference.java
@@ -3,20 +3,20 @@ package com.frcteam3255.preferences;
 /**
  * SuperNURDs double zero preference base class
  */
-public class SN_ZeroIntPreference extends SN_IntPreference{
+public class SN_ZeroIntPreference extends SN_IntPreference {
 
-    /**
-     * Creates a preference with name "doubleZeroPreference" and value 0
-     */
-    public SN_ZeroIntPreference() {
-        super("intZeroPreference", 0);
-    }
+	/**
+	 * Creates a preference with name "doubleZeroPreference" and value 0
+	 */
+	public SN_ZeroIntPreference() {
+		super("intZeroPreference", 0);
+	}
 
-    /**
-     * @return 0, type int
-     */
-    @Override
-    public int getValue() {
-        return 0;
-    }
+	/**
+	 * @return 0, type int
+	 */
+	@Override
+	public int getValue() {
+		return 0;
+	}
 }

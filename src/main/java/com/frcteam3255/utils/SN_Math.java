3b428cc1b88ccce92497248a25794118bdcb4f6a
IanSzalai <60115484+IanSzalai@users.noreply.github.com>
2022-02-21 18:19:47 -0800

    added SN_RPM with methods to convert to/from RPM (#35)
    * added SN_RPM with methods to convert to/from RPM
    
    * spotless apply
    
    * updated SN_RPM
    
    * moved SN_RPM methods to SN_Math
    
    * velocityToRPM throws exception instead of return

--- src/main/java/com/frcteam3255/utils/SN_Math.java
+++ src/main/java/com/frcteam3255/utils/SN_Math.java
@@ -39,4 +39,47 @@ public class SN_Math {
 		}
 		return output;
 	}
+
+	public static final double TALONFX_ENCODER_PULSES_PER_COUNT = 2048;
+	public static final double QUAD_ENCODER_PULSES_PER_COUNT = 4096;
+
+	/**
+	 * Converts Velocity to RPM
+	 *
+	 * @param a_velocity
+	 *            Motor encoder counts per 100ms
+	 * @param a_pulsesPerCount
+	 *            Encoder counts per revolution
+	 * @return Motor RPM
+	 */
+	public static double velocityToRPM(double a_velocity, int a_pulsesPerCount) {
+
+		try {
+			double rpm = a_velocity; // counts per 100ms
+			rpm *= 10; // counts per 1s
+			rpm *= 60; // counts per 1m
+			rpm /= a_pulsesPerCount; // rotations per minute
+			return rpm;
+		} catch (ArithmeticException e) {
+			System.err.println("pulsesPerCount can not be 0");
+			throw e;
+		}
+	}
+
+	/**
+	 * Converts RPM to velocity
+	 *
+	 * @param a_rpm
+	 *            Motor RPM
+	 * @param a_pulsesPerCount
+	 *            Encoder counts per revolution
+	 * @return Motor encoder counts per 100ms
+	 */
+	public static double RPMToVelocity(double a_rpm, int a_pulsesPerCount) {
+		double velocity = a_rpm; // rotations per 1m
+		velocity *= a_pulsesPerCount; // counts 1m
+		velocity /= 60; // counts per 1s
+		velocity /= 10; // counts per 100ms
+		return velocity;
+	}
 }

955be06bd4edee53b65c01abbdd37b11eddbd3ff
IanSzalai <60115484+IanSzalai@users.noreply.github.com>
2022-02-21 18:12:41 -0800

    updated SN_RPM

--- src/main/java/com/frcteam3255/utils/SN_RPM.java
+++ src/main/java/com/frcteam3255/utils/SN_RPM.java
@@ -2,33 +2,44 @@ package com.frcteam3255.utils;
 
 public class SN_RPM {
 
-	private static final double TALONFX_COUNTS_PER_ROTATION = 2048;
+	public static final double TALONFX_ENCODER_PULSES_PER_COUNT = 2048;
+	public static final double QUAD_ENCODER_PULSES_PER_COUNT = 4096;
 
 	/**
-	 * Converts TalonFX Velocity to RPM
+	 * Converts Velocity to RPM
 	 *
 	 * @param a_velocity
 	 *            Motor encoder counts per 100ms
+	 * @param a_pulsesPerCount
+	 *            Encoder counts per revolution
 	 * @return Motor RPM
 	 */
-	public static double talonFXVelocityToRPM(double a_velocity) {
-		double rpm = a_velocity; // counts per 100ms
-		rpm *= 10; // counts per 1s
-		rpm *= 60; // counts per 1m
-		rpm /= TALONFX_COUNTS_PER_ROTATION; // rotations per minute
-		return rpm;
+	public static double velocityToRPM(double a_velocity, int a_pulsesPerCount) {
+
+		try {
+			double rpm = a_velocity; // counts per 100ms
+			rpm *= 10; // counts per 1s
+			rpm *= 60; // counts per 1m
+			rpm /= a_pulsesPerCount; // rotations per minute
+			return rpm;
+		} catch (ArithmeticException e) {
+			System.err.println("pulsesPerCount can not be 0");
+			return 0;
+		}
 	}
 
 	/**
-	 * Converts RPM to TalonFX velocity
+	 * Converts RPM to velocity
 	 *
 	 * @param a_rpm
 	 *            Motor RPM
+	 * @param a_pulsesPerCount
+	 *            Encoder counts per revolution
 	 * @return Motor encoder counts per 100ms
 	 */
-	public static double talonFXRPMToVelocity(double a_rpm) {
+	public static double RPMToVelocity(double a_rpm, int a_pulsesPerCount) {
 		double velocity = a_rpm; // rotations per 1m
-		velocity *= TALONFX_COUNTS_PER_ROTATION; // counts 1m
+		velocity *= a_pulsesPerCount; // counts 1m
 		velocity /= 60; // counts per 1s
 		velocity /= 10; // counts per 100ms
 		return velocity;

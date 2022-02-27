9891da1285aed00f879e6466534e4c526df7b253
IanSzalai <60115484+IanSzalai@users.noreply.github.com>
2022-02-26 17:21:07 -0800

    made thing work

--- src/main/java/com/frcteam3255/components/SN_DoubleSolenoid.java
+++ src/main/java/com/frcteam3255/components/SN_DoubleSolenoid.java
@@ -1,28 +1,34 @@
 package com.frcteam3255.components;
 
-import com.frcteam3255.preferences.SN_BooleanPreference;
-
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 
 public class SN_DoubleSolenoid extends DoubleSolenoid {
 
-	DoubleSolenoid solenoid;
-	boolean inverted;
+	Value forwardValue;
+	Value reverseValue;
 
-	Value forwardValue = Value.kForward;
-	Value reverseValue = Value.kReverse;
+	
+	/**
+	 * Wrapper for the WPILib DoubleSolenoid
+	 * <p>
+	 * Adds custom methods like {@link #setDeployed()} and {@link #getDeployed()}
+	 *
+	 * @param a_moduleType
+	 *            CTRE Pneumatics Control Module or REV Pneumatics Hub
+	 * @param a_forwardChannel
+	 *            The forward channel of the solenoid on the Pneumatics Module
+	 * @param a_reverseChannel
+	 *            The reverse channel of the solenoid on the Pneumatics Module
+	 */
+	public SN_DoubleSolenoid(PneumaticsModuleType a_moduleType, int a_forwardChannel, int a_reverseChannel) {
+		super(a_moduleType, a_forwardChannel, a_reverseChannel);
+		forwardValue = Value.kForward;
+		reverseValue = Value.kReverse;
+	}
 
 	private Value getValue() {
-		if (!inverted) {
-			return solenoid.get();
-		} else {
-			if (solenoid.get() == forwardValue) {
-				return reverseValue;
-			} else {
-				return forwardValue;
-			}
-		}
+		return this.get();
 	}
 
 	/**
@@ -40,19 +46,11 @@ public class SN_DoubleSolenoid extends DoubleSolenoid {
 	}
 
 	private void rawSetDeployed() {
-		if (!inverted) {
-			solenoid.set(forwardValue);
-		} else {
-			solenoid.set(reverseValue);
-		}
+		this.set(forwardValue);
 	}
 
 	private void rawSetReverse() {
-		if (inverted) {
-			solenoid.set(reverseValue);
-		} else {
-			solenoid.set(forwardValue);
-		}
+		this.set(reverseValue);
 	}
 
 	/**
@@ -86,66 +84,13 @@ public class SN_DoubleSolenoid extends DoubleSolenoid {
 		}
 	}
 
-	private boolean getBoolPrefValue(SN_BooleanPreference a_pref) {
-		return a_pref.getValue();
-	}
-
-	/**
-	 * Wrapper for the WPILib DoubleSolenoid
-	 * <p>
-	 * Adds custom methods like {@link #setDeployed()} and {@link #getDeployed()}
-	 *
-	 * @param a_moduleType
-	 *            CTRE Pneumatics Control Module or REV Pneumatics Hub
-	 * @param a_forwardChannel
-	 *            The forward channel of the solenoid on the Pneumatics Module
-	 * @param a_reverseChannel
-	 *            The reverse channel of the solenoid on the Pneumatics Module
-	 */
-	public SN_DoubleSolenoid(PneumaticsModuleType a_moduleType, int a_forwardChannel, int a_reverseChannel) {
-		super(a_moduleType, a_forwardChannel, a_reverseChannel);
-		inverted = false;
-	}
-
-	/**
-	 * Wrapper for the WPILib DoubleSolenoid
-	 * <p>
-	 * Adds custom methods like {@link #setDeployed()} and {@link #getDeployed()}
-	 *
-	 * @param a_moduleType
-	 *            CTRE Pneumatics Control Module or REV Pneumatics Hub
-	 * @param a_forwardChannel
-	 *            The forward channel of the solenoid on the Pneumatics Module
-	 * @param a_reverseChannel
-	 *            The reverse channel of the solenoid on the Pneumatics Module
-	 * @param a_inverted
-	 *            Inverts the direction of the Solenoid (equivalent to physically
-	 *            swapping the forward and reverse channels)
-	 */
-	public SN_DoubleSolenoid(PneumaticsModuleType a_moduleType, int a_forwardChannel, int a_reverseChannel,
-			boolean a_inverted) {
-		super(a_moduleType, a_forwardChannel, a_reverseChannel);
-		inverted = a_inverted;
-	}
-
-	/**
-	 * Wrapper for the WPILib DoubleSolenoid
-	 * <p>
-	 * Adds custom methods like {@link #setDeployed()} and {@link #getDeployed()}
-	 *
-	 * @param a_moduleType
-	 *            CTRE Pneumatics Control Module or REV Pneumatics Hub
-	 * @param a_forwardChannel
-	 *            The forward channel of the solenoid on the Pneumatics Module
-	 * @param a_reverseChannel
-	 *            The reverse channel of the solenoid on the Pneumatics Module
-	 * @param a_inverted
-	 *            Inverts the direction of the Solenoid (equivalent to physically
-	 *            swapping the forward and reverse channels)
-	 */
-	public SN_DoubleSolenoid(PneumaticsModuleType a_moduleType, int a_forwardChannel, int a_reverseChannel,
-			SN_BooleanPreference a_inverted) {
-		super(a_moduleType, a_forwardChannel, a_reverseChannel);
-		inverted = getBoolPrefValue(a_inverted);
+	public void invert() {
+		if (forwardValue == Value.kForward) {
+			forwardValue = Value.kReverse;
+			reverseValue = Value.kForward;
+		} else {
+			forwardValue = Value.kForward;
+			reverseValue = Value.kReverse;
+		}
 	}
 }

50133dc6724528bb1a49e434950f3bb743cb5fc9
Tayler Uva <8679670+TaylerUva@users.noreply.github.com>
2022-03-05 14:23:16 -0800

    Removed releases folder (#42)
    * deleted releases folder from main
    
    * update gitignore

--- releases/com/frcteam3255/supercore/0.0.5/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.frcteam3255.components"},{"l":"com.frcteam3255.joystick"},{"l":"com.frcteam3255.preferences"},{"l":"com.frcteam3255.utils"}]

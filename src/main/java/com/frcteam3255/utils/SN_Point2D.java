c400477a727dcf736ae0689ace740c1fa31fba7a
IanSzalai <60115484+IanSzalai@users.noreply.github.com>
2022-10-28 19:45:26 -0700

    Create SN_Point2D and updated improved LinearInterpolationTable to use it (#64)
    * create SN_Point2D
    
    * update LinearInterpolationTable to use SN_Point2D

---
+++ src/main/java/com/frcteam3255/utils/SN_Point2D.java
@@ -0,0 +1,21 @@
+package com.frcteam3255.utils;
+
+public class SN_Point2D {
+
+  double m_x;
+  double m_y;
+
+  public SN_Point2D(double x, double y) {
+    m_x = x;
+    m_y = y;
+  }
+
+  public double getX() {
+    return m_x;
+  }
+
+  public double getY() {
+    return m_y;
+  }
+  
+}

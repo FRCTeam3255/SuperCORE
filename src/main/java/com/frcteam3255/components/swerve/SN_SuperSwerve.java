62715775d444b3919efa6f1bf49df919d78028b1
ACat701 <90939494+ACat701@users.noreply.github.com>
2024-08-03 02:19:28 -0700

    Reset simAngle with Pigeon angle

--- src/main/java/com/frcteam3255/components/swerve/SN_SuperSwerve.java
+++ src/main/java/com/frcteam3255/components/swerve/SN_SuperSwerve.java
@@ -28,6 +28,7 @@ import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -400,6 +401,10 @@ public class SN_SuperSwerve extends SubsystemBase {
 	 * Resets the Yaw of the Pigeon to 0
 	 */
 	public void resetYaw() {
+		if (isSimulation) {
+			simAngle = 0;
+		}
+
 		pigeon.setYaw(0);
 	}
 
@@ -410,6 +415,10 @@ public class SN_SuperSwerve extends SubsystemBase {
 	 *            The yaw (in degrees) to reset the Pigeon to
 	 */
 	public void resetYaw(double yaw) {
+		if (isSimulation) {
+			simAngle = Units.degreesToRadians(yaw);
+		}
+
 		pigeon.setYaw(yaw);
 	}
 

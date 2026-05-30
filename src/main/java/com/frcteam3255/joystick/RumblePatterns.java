df0a10a5df131915be9a78b202c0d820752d92dc
BrodyKarr <boocat1030@icloud.com>
2026-05-30 16:38:25 -0700

    cleaned up logic

--- src/main/java/com/frcteam3255/joystick/RumblePatterns.java
+++ src/main/java/com/frcteam3255/joystick/RumblePatterns.java
@@ -10,32 +10,29 @@ import edu.wpi.first.wpilibj.Timer;
 
 /** Add your docs here. */
 public class RumblePatterns {
-    private final RumbleType rumbleType;
-    private final double intensity;
+    public final RumbleType rumbleType;
+    public final double intensity;
 
     public RumblePatterns(RumbleType rumbleType, double intensity) {
         this.rumbleType = rumbleType;
         this.intensity = intensity;
     }
 
-    public static RumblePatterns getRumblePattern(RumbleType rumbleType, double rumbleIntensity) {
+    public RumblePatterns getRumblePattern(String selectedRumble) {
         // Replace this with your preference system as needed; using a plain String for compilation
-        String selectedRumble = "Steady";
         double t = Timer.getFPGATimestamp(); // seconds since FPGA boot
         boolean toggle = ((int) Math.floor(t) % 2) == 0; // toggle every 1 second
-        final RumblePatterns steadyController = new RumblePatterns(rumbleType, rumbleIntensity);
-        final RumblePatterns buzzController = new RumblePatterns(rumbleType, rumbleIntensity);
-        final RumblePatterns flickerController = new RumblePatterns(rumbleType, toggle ? rumbleIntensity : 0);
+        final RumblePatterns steadyController = new RumblePatterns(rumbleType, 0);
+        final RumblePatterns buzzController = new RumblePatterns(rumbleType, intensity);
+        final RumblePatterns flickerController = new RumblePatterns(rumbleType, toggle ? intensity : 0);
 
-        if ("Steady".equals(selectedRumble)) {
-            return steadyController;
-        } else if ("Buzz".equals(selectedRumble)) {
+        if ("Buzz".equals(selectedRumble)) {
             return buzzController;
         } else if ("Flicker".equals(selectedRumble)) {
             return flickerController;
         }
-
-        // default
+        else {
         return steadyController;
+        }
     }
 }
